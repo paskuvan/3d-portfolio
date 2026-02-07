c26a3a70995f534cede3f6f9e2e7d2cd1e58c1bf
paskuvan <majopaskuvan@gmail.com>
2026-02-07 01:00:39 -0300

    Implement feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
 import LogoSection from "./components/LogoSection";
+import FeatureCards from "./sections/FeatureCards";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
    <Hero />
    <ShowcaseSection />
    <LogoSection />
+   <FeatureCards />
    </>
   )
 }
